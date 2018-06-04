274ad0a2137802fdf6d68d63d7c2e93ddd3c3f8a
parenparen <dtmuelle@gmail.com>
2018-06-03 19:30:22 -0700

    added reset button to 01

--- examples/01-button.elm
+++ examples/01-button.elm
@@ -1,26 +1,28 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 
-
 main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
 
 
 
 -- MODEL
 
 
-type alias Model = Int
+type alias Model =
+    Int
 
 
 model : Model
 model =
-  0
+    0
 
 
 
@@ -28,18 +30,22 @@ model =
 
 
 type Msg
-  = Increment
-  | Decrement
+    = Increment
+    | Decrement
+    | Reset
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
+
+        Reset ->
+            0
 
 
 
@@ -48,8 +54,9 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Reset ] [ text "Reset" ]
+        ]
